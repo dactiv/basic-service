2fd94139a50dca19c6d045c7a8d2534a47105b20
chenxiaobo <maurice.chen@foxmail.com>
2021-12-15 17:28:29 +0800

    登录成功，保存一次代码。

--- gateway/src/main/java/com/github/dactiv/basic/gateway/GatewayMain.java
+++ gateway/src/main/java/com/github/dactiv/basic/gateway/GatewayMain.java
@@ -3,11 +3,17 @@ package com.github.dactiv.basic.gateway;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.dactiv.basic.gateway.config.ApplicationConfig;
 import com.github.dactiv.framework.commons.Casts;
+import com.github.dactiv.framework.spring.security.SpringSecurityAutoConfiguration;
 import com.github.dactiv.framework.spring.web.SpringWebMvcProperties;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.reactive.ReactiveManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.boot.autoconfigure.security.reactive.ReactiveSecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.reactive.ReactiveUserDetailsServiceAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SpringBootWebSecurityConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
@@ -16,6 +22,7 @@ import org.springframework.core.annotation.Order;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 import java.util.stream.Collectors;
 
@@ -27,7 +34,13 @@ import java.util.stream.Collectors;
 @EnableScheduling
 @EnableDiscoveryClient
 @EnableFeignClients("com.github.dactiv.basic.commons.feign")
-@SpringBootApplication(scanBasePackages = "com.github.dactiv.basic.gateway")
+@SpringBootApplication(
+        scanBasePackages = "com.github.dactiv.basic.gateway",
+        exclude = {
+                ReactiveSecurityAutoConfiguration.class,
+                ReactiveManagementWebSecurityAutoConfiguration.class,
+                ReactiveUserDetailsServiceAutoConfiguration.class
+        })
 public class GatewayMain {
 
     public static void main(String[] args) {
